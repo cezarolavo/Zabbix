8f687acc571a2213ba465eeb45c3bbcd273ec200
cezarolavo <49735379+cezarolavo@users.noreply.github.com>
2021-06-24 17:30:48 -0300

    Update lista_sites.sh

--- lista_sites.sh
+++ lista_sites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script para obter sites a monitorar, inserindo em regra autodiscovery no Zabbix
-filename=/usr/lib/zabbix/externalscripts/lista_sites_web
+filename=/etc/zabbix/externalscripts/lista_sites_web
 while IFS="," read -r aplicacao hostname url
 do
    echo "$aplicacao"";""$hostname"";""$url"
