83f671cc1d52db9ea555426732757b475901acd0
cezarolavo <49735379+cezarolavo@users.noreply.github.com>
2021-10-28 22:57:16 -0300

    Update lista_ip_traceroute.sh

--- lista_ip_traceroute.sh
+++ lista_ip_traceroute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script para obter IPs para traceroute, inserindo em regra autodiscovery no Zabbix
 filename=/etc/zabbix/externalscripts/lista_ip_traceroute
-while IFS="," read -r aplicacao hostname ip
+while IFS="," read -r aplicacao site ip
 do
-   echo "$aplicacao"";""$hostname"";""$ip"
+   echo "$aplicacao"";""$site"";""$ip"
 done < $filename
